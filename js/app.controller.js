1ac6d648c1ce6e8517b6f6d624676f3196bf11e7
Bew Harichanwong <bew.hari@gmail.com>
2016-02-25 20:27:21 -0600

    Add congress members list

--- js/app.controller.js
+++ js/app.controller.js
@@ -59,8 +59,56 @@ angular.module('team535.controllers', [])
     };
 
     $scope.savedDistricts = [
-      { 'id': 1, 'name': 'Illinois\' 9th' },
-      { 'id': 2, 'name': 'Illinois\' 10th' }
+      { 
+        'id': 19, 
+        'name': 'Illinois\'s 9th',
+        'senators': [
+        {
+          'id': 1023,
+          'rank': 'senior',
+          'first_name': 'Dick',
+          'last_name': 'Durbin',
+          'party': 'Democrat'
+        },
+        {
+          'id': 4976,
+          'rank': 'junior',
+          'first_name': 'Mark',
+          'last_name': 'Kirk',
+          'party': 'Republican'
+        }],
+        'representative': {
+          'id': 2231,
+          'first_name': 'Jan',
+          'last_name': 'Schakowsky',
+          'party': 'Democrat'
+        }
+      },
+      { 
+        'id': 22, 
+        'name': 'Ohio\'s 2nd',
+        'senators': [
+        {
+          'id': 2993,
+          'rank': 'senior',
+          'first_name': 'Sherrod',
+          'last_name': 'Brown',
+          'party': 'Democrat'
+        },
+        {
+          'id': 5302,
+          'rank': 'junior',
+          'first_name': 'Rob',
+          'last_name': 'Portman',
+          'party': 'Republican'
+        }],
+        'representative': {
+          'id': 3054,
+          'first_name': 'Brad',
+          'last_name': 'Wenstrup',
+          'party': 'Republican'
+        }
+      },
     ];
 
     $scope.district = $scope.savedDistricts[0];
