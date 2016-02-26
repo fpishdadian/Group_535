c5b5523b7b344f57928e529b8b65996665aff3c3
Bew Harichanwong <bew.hari@gmail.com>
2016-02-26 04:45:04 -0600

    Link to member page from district page

--- js/district.controller.js
+++ js/district.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('team535.controllers')
-  .controller('DistrictCtrl', function($scope, $stateParams, DistrictService) {
+  .controller('DistrictCtrl', function($scope, $state, $stateParams, DistrictService) {
     $scope.controllerName = 'DistrictCtrl';
     $scope.id = $stateParams.id;
 
     $scope.district = DistrictService.get({id: $stateParams.id});
+
+    $scope.selectMember = function(id) {
+    	$state.go('app.member', {id: id});
+    };
   });
